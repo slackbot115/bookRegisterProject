4b5dd6e945538749c698d81402aebb8876111b39
luskas8 <gnomo1049@gmail.com>
2019-06-16 10:12:20 +0900

    Configurando ação de cadastro

--- www/components/actions.js
+++ www/components/actions.js
@@ -1,2 +1,11 @@
 // This is a JavaScript file
 
+$('#cadastro').click(function(){
+    var parametros = {
+      'titulo': $('#titulo').val(),
+      'autor': $('#autor').val(),
+      'ano': $('#ano').val(),
+      'isbm': $('#isbm').val()
+    }
+    alert(parametros)
+  })
